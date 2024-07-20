47b0024447e4c915eb818bb7905b3d369621bd56
laobamac <107761278+laobamac@users.noreply.github.com>
2024-07-20 20:44:17 +0800

    Update install_dependences.py

--- install_dependences.py
+++ install_dependences.py
@@ -6,8 +6,8 @@ def install(package):
 
 if __name__ == "__main__":
     # 列出所有需要安装的依赖
-    dependencies = ['tqdm', 'cpuinfo', 'shutil', 'requests', 'psutil', 'gpuinfo']
+    dependencies = ['tqdm', 'cpuinfo', 'shutil', 'requests', 'psutil', 'gpuinfo', 'zipfile', 'os']
 
     # 使用 pip 安装依赖
     for package in dependencies:
-        install('pip3 install {}'.format(package))
+        install('pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple {}'.format(package))
